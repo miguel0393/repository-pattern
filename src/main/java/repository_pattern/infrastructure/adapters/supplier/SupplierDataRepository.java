85f4f2495116de03c8db53df96c70d240b7d03f0
Miguel <miguel.bolivar@sofka.com.co>
2020-03-01 22:11:10 -0500

    refactor

--- src/main/java/repository_pattern/infrastructure/adapters/supplier/SupplierDataRepository.java
+++ src/main/java/repository_pattern/infrastructure/adapters/supplier/SupplierDataRepository.java
@@ -1,6 +1,9 @@
 package repository_pattern.infrastructure.adapters.supplier;
 
-public interface SupplierDataRepository {
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import repository_pattern.domain.model.supplier.Supplier;
+
+public interface SupplierDataRepository extends ReactiveCrudRepository<Supplier, String> {
 
 
 }
--- src/main/java/repository_pattern/infrastructure/adapters/supplier/SupplierMongoAdapter.java
+++ src/main/java/repository_pattern/infrastructure/adapters/supplier/SupplierMongoAdapter.java
@@ -1,7 +1,23 @@
 package repository_pattern.infrastructure.adapters.supplier;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+import repository_pattern.domain.model.supplier.Supplier;
+import repository_pattern.domain.model.supplier.SupplierRepository;
 
 @Repository
-public class SupplierMongoAdapter {
+public class SupplierMongoAdapter implements SupplierRepository {
+
+    private final SupplierDataRepository repository;
+
+    @Autowired
+    public SupplierMongoAdapter(SupplierDataRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Mono<Supplier> getSupplierById(String id) {
+        return repository.findById(id);
+    }
 }
